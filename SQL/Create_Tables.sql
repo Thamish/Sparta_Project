c264a8008871664201b82f522371f646c31b24e8
thamish perera <thamishperera24@yahoo.co.uk>
2020-06-16 07:44:51 +0100

    SQL Queries

---
+++ SQL/Create_Tables.sql
@@ -0,0 +1,28 @@
+﻿--//Delete Tables
+--DROP TABLE Players;
+--DROP TABLE Clubs;
+--DROP TABLE Positions;
+
+--//Create Tables
+CREATE TABLE Positions
+(
+	positionID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	positionDescription VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE Clubs
+(
+	clubID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	clubname VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Players
+(
+	playerID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	firstName VARCHAR(50),
+	lastName VARCHAR(50),
+	positionId INT REFERENCES Positions(positionID),
+	nationality VARCHAR(50),
+	clubId INT REFERENCES Clubs(ClubID),
+	dateOfBirth DATE
+);
---
+++ SQL/Populate_Tables.sql
@@ -0,0 +1,37 @@
+﻿--//Insert data into tables
+INSERT INTO Positions VALUES
+('GoalKeeper'),
+('Defender'),
+('Midfielder'),
+('Forward');
+
+INSERT INTO Clubs VALUES
+('Arsenal'),
+('Chelsea'),
+('Liverpool'),
+('Manchester City'),
+('Manchester United');
+
+INSERT INTO Players VALUES
+('Bernd','Leno',1,'Germany',1,'1992/03/04'),
+('Héctor','Bellerin',2,'Spain',1,'1995/03/19'),
+('Shkodran','Mustafi',2,'Germany',1,'1992/04/17'),
+('David','Luiz',2,'Brazil',1,'1987/04/22'),
+('Bukayo','Saka',2,'England',1,'2001/09/05'),
+('Mesut','Özil',3,'Germany',1,'1988/10/15'),
+('Lucas','Torreira',3,'Uruguay',1,'1996/02/11'),
+('Nicolas','Pépé',3,'Cote D''lvoire',1,'1995/05/29'),
+('Granit','Xhaka',3,'Switzerland',1,'1992/09/27'),
+('Gabriel','Martinelli',3,'Brazil',1,'2001/06/18'),
+('Alexandre','Lacazette',4,'France',1,'1991/05/28'),
+('Willy','Caballero',1,'Argentina',2,'1995/05/29'),
+('Kurt','Zouma',2,'France',2,'1994/10/27'),
+('Andreas','Christensen',2,'Denmark',2,'1996/04/10'),
+('Reece','James',2,'England',2,'1999/12/08'),
+('César','Azpilicueta',2,'Spain',2,'1989/08/28'),
+('Jorginho','',3,'Italy',2,'1991/12/20'),
+('N''Golo','Kanté',3,'France',2,'1991/03/29'),
+('Mateo','Kovacic',3,'Croatia',2,'1994/05/06'),
+('Willian','',4,'Brazil',2,'1988/08/09'),
+('Pedro','',4,'Spain',2,'1987/07/28'),
+('Olivier','Giroud',4,'France',2,'1986/09/30');
